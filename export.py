eac646087a0c2a7f35c07e8d2aa3f59e8ad9b64d
ruzen42 <malledeferlive@gmailcom>
2025-01-02 13:05:44 +0500

    new year 2025

--- export.py
+++ export.py
@@ -1,6 +1,6 @@
 import os
 if os.system("dotnet build --verbosity quiet owao/OWASDsharp.csproj") == 0:
-    godot3 --export "windows" binares/OWASD-win.exe
-    godot3 --export "linux" binares/OWASD-linux.x86_64
+    os.system("cd owao && /home/ruzen42/Documents/Godot/Godot.x86_64 --export-debug \"windows\" binares/OWASD-win.exe")
+    os.system("cd owao && /home/ruzen42/Documents/Godot/Godot.x86_64 --export-debug \"linux\" binares/OWASD-linux.x86_64")
 else:
     print("Bad")
