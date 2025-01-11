8f5f349f89d8336137b13fe191eb3a569b4bd73b
Deny ruzen <malledeferlive@gmail.com>
2025-01-11 13:52:08 +0500

    add mime sprites and fix him camera logic

--- src/Code/Mime.cs
+++ src/Code/Mime.cs
@@ -5,21 +5,28 @@ public partial class Mime : Sprite2D
 {
 	SceneTree sceneTree;
 	PackedScene newScene;
+	Texture2D Petlu;
 
 	public override void _Ready()
 	{
 		if (saves.NightSelected < 3) 
 			QueueFree();
+		Petlu = (Texture2D)ResourceLoader.Load("res://Sprites/Mime/MimeSprite1.png");
 	}
 	
 	public override void _Process(double delta)
 	{
+		if (saves.SelectedCamera == 5)
+			Visible = true;
+		else 
+			Visible = false;
 		if (saves.Noise > 60)
 			Kill();
 	}
 
 	private void Kill()
 	{
+		Texture = Petlu;
 		GetNode<AudioStreamPlayer>("../SoundForKill").Play();
 	}
 	
