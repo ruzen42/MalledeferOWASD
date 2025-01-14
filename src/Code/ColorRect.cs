452eaad5a4ef095e4bf9ab43b66d6a7488f189a2
Deny ruzen <malledeferlive@gmail.com>
2025-01-14 16:48:39 +0500

    fix color rect logic

--- src/Code/ColorRect.cs
+++ src/Code/ColorRect.cs
@@ -3,23 +3,25 @@ using Godot;
 // суть этого класса чтобы создать эффект появления из мрака при запуске игры
 public partial class ColorRect : Godot.ColorRect
 {
-	// Переменные для удобства эффекта
-	private int modulate = 255;
-	private bool hasCompleted = false;
+  // Переменные для удобства эффекта
+  private int modulate = 255;
+  private bool hasCompleted = false;
 
-	// Придание стандартных свойств
-	public override void _Ready()
+  // Придание стандартных свойств
+  public override void _Ready()
+  {
+	Visible = true;
+	Modulate = new Color(1, 1, 1, 255);
+  }
+  // Сам эффект
+  public override void _Process(double delta)
+  {
+	if (modulate <= 0)
+	  Visible = false;
+	else
 	{
-		Visible = true;
-		Modulate = new Color(1, 1, 1, 255);
+	  Modulate = new Color(1, 1, 1, modulate / 255f);
+	  modulate = modulate - 1;
 	}
-	// Сам эффект
-	public override void _Process(double delta)
-	{
-		modulate = modulate - 1;
-		if (modulate < 0)
-			Visible = false;
-		else
-			Modulate = new Color(1, 1, 1, modulate / 255f);
-	}
+  }
 }
