70145a58b551c88b15a78620c0e1ed1d2b167e36
ruzen42 <malledeferlive@gmail.com>
2025-01-19 10:39:56 +0500

    add mini_game

---
+++ src/Code/CharacterBody2d.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class CharacterBody2d : CharacterBody2D
+{
+	public const float Speed = 600.0f;
+	public const float JumpVelocity = -700.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
--- src/Code/ColorRect.cs
+++ src/Code/ColorRect.cs
@@ -1,26 +1,14 @@
 using Godot;
 
-// суть этого класса чтобы создать эффект появления из мрака при запуске игры
+
 public partial class ColorRect : Godot.ColorRect
 {
-  // Переменные для удобства эффекта
-	private float modulate = 255;
+	private float alpha = 255;
 	private bool hasCompleted = false;
-
-  // Придание стандартных свойств
-	  public override void _Ready()
-	  {
-		Visible = true;
-	  }
-  // Сам эффект
-  public override void _Process(double delta)
-  {
-	if (modulate <= 0)
-	 Visible = false;
-	else
+	
+	public override void _Process(double delta) 
 	{
-	  modulate = 0;
-	  Color = new Color(0, 0, 0, modulate);
+		Color = new Color(0, 0, 0, alpha / 255f);
+		alpha--;
 	}
-  }
 }
